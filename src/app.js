54938f4616dc65c90f88d17d88488ca70cda6a04
Natacha <natachaliao32@gmail.com>
2022-04-23 15:25:10 +0200

    comment

--- src/app.js
+++ src/app.js
@@ -4,22 +4,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const bodyParser = require("body-parser");
-const {router: projectsRoute} = require("../functions/projects.js");
-const {router: categoriesRoute} = require("../functions/categories.js");
-const {router: toolsRoute} = require("../functions/tools.js");
-const {router: filesRoute} = require("../functions/files.js");
 const cors = require('cors');
 const upload = require('express-fileupload');
 const serverless = require("serverless-http");
+// const {router: projectsRoute} = require("../functions/projects.js");
+// const {router: categoriesRoute} = require("../functions/categories.js");
+// const {router: toolsRoute} = require("../functions/tools.js");
+// const {router: filesRoute} = require("../functions/files.js");
 
 const app = express();
 
 // MIDDLEWARES
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use(upload());
-app.use("/.netlify/functions/files", express.static("assets"));
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use(upload());
+// app.use("/.netlify/functions/files", express.static("assets"));
 
 const router = express.Router();
 
@@ -32,10 +32,10 @@ app.use('/.netlify/functions/app', router);
 
 // ROUTES
 
-app.use("/.netlify/functions/app/projects", projectsRoute);
-app.use("/.netlify/functions/app/categories", categoriesRoute);
-app.use("/.netlify/functions/app/tools", toolsRoute);
-app.use("/.netlify/functions/app/files", filesRoute);
+// app.use("/.netlify/functions/app/projects", projectsRoute);
+// app.use("/.netlify/functions/app/categories", categoriesRoute);
+// app.use("/.netlify/functions/app/tools", toolsRoute);
+// app.use("/.netlify/functions/app/files", filesRoute);
 
 
 // app.listen(3000, () => console.log("App is running on http://localhost:3000"));
